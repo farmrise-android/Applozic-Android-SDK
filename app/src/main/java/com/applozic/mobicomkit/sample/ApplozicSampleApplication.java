5266a733c9022432513cd7a3c97f6c7559d74dbe
Sai Kiran Reddy Patlolla <saikiran.patlolla@yash.com>
2018-03-28 16:55:23 +0530

    Code refactoring in Application class

--- app/src/main/java/com/applozic/mobicomkit/sample/ApplozicSampleApplication.java
+++ app/src/main/java/com/applozic/mobicomkit/sample/ApplozicSampleApplication.java
@@ -3,13 +3,19 @@ package com.applozic.mobicomkit.sample;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
+import android.text.TextUtils;
+
+import com.applozic.mobicommons.people.ALContactProcessor;
 import com.crashlytics.android.Crashlytics;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber;
+
 import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by sunil on 21/3/16.
  */
-public class ApplozicSampleApplication extends MultiDexApplication {
+public class ApplozicSampleApplication extends MultiDexApplication implements ALContactProcessor {
 
     @Override
     public void onCreate() {
@@ -22,6 +28,39 @@ public class ApplozicSampleApplication extends MultiDexApplication {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         MultiDex.install(this);
+    }
+
+    @Override
+    public String processContact(String number, String defaultCountryCode) {
+        if (TextUtils.isEmpty(number)) {
+            return "";
+        }
+
+        Phonenumber.PhoneNumber phoneNumber;
+        PhoneNumberUtil phoneUtil = PhoneNumberUtil.getInstance();
+
+        if (TextUtils.isEmpty(number)) {
+            return "";
+        }
+
+        long contactNumber = 0;
+        int countryCode = 0;
+
+        try {
+            phoneNumber = phoneUtil.parse(number, defaultCountryCode);
+            if (phoneNumber.hasCountryCode()) {
+                countryCode = phoneNumber.getCountryCode();
+            }
+
+            contactNumber = phoneNumber.getNationalNumber();
+        } catch (Exception ex) {
+            try {
+                contactNumber = Long.parseLong(number);
+            } catch (Exception e) {
+                return number;
+            }
+        }
+        return "+" + countryCode + contactNumber;
     }
 
 
